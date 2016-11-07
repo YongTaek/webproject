0810cc775aa00acb581f8b96841b765646f4f10a
Cheon Yu Jung <chyjis@naver.com>
2016-11-07 17:36:35 +0900

    notice와 free list 완성

--- view/noticelist.php
+++ view/noticelist.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
 	<link rel="stylesheet" href="../public/css/bootstrap.min.css" type="text/css">
-	<link rel="stylesheet" type="text/css" href="../public/css/questionlist.css">
+	<link rel="stylesheet" type="text/css" href="../public/css/noticelist.css">
 	<link rel="stylesheet" href="../public/css/base.css" type="text/css">
 
 	<meta charset="utf-8">
-	<title>질문 게시판</title>
+	<title>공지 게시판</title>
 </head>
 <body>
 	<header role ="banner">
@@ -15,8 +15,8 @@
 				<a href="/"><img class="logo" src="selab_logo_S.png" /></a>
 			</div>
 			<ul id="menu" class="inline-list pull-left">
-				<li class="pull-left"><a href="/view/noticelist.php" class="menu-item" >NOTICE</a></li>
-				<li class="pull-left"><a href="/view/questionlist.php" class="active menu-item">QUESTION</a></li>
+				<li class="pull-left"><a href="/view/noticelist.php" class="active menu-item" >NOTICE</a></li>
+				<li class="pull-left"><a href="/view/questionlist.php" class="menu-item">QUESTION</a></li>
 				<li class="pull-left"><a href="/view/freelist.php" class="menu-item">FREE BOARD</a></li>
 			</ul>
 			<div role="login" class="pull-right">
@@ -25,12 +25,12 @@
 		</nav>
 	</header><!-- /header -->
 	<div class = "jumbotron banner-color">
-		<h1 class="align-center">Questions</h1>
+		<h1 class="align-center">NOTICE</h1>
 		<p class="lead align-center">Wed 3:30 ~ & Thu 10:30 ~ </p>
 	</div>
 	<div class= "content">
 		<div class="subheader">
-			<h2>ALL QUESTION</h2>
+			<h2>ALL NOTICE</h2>
 			<ul class="nav nav-tabs">
 				<li class="question-tab active"><a herf = "/recent">recent</a></li>
 				<li class="question-tab"><a href = "/recommend">recommend</a></li>
@@ -45,28 +45,12 @@
 						</div>
 						<div>indexs</div>
 					</div>
-					<div class= "votes-number">
-						<div class= "mini-count">
-							<span>0</span> <!-- 추천 수  -->
-						</div>
-						<div>votes</div>
-					</div>
-					<div class= "answer-number">
-						<div class= "mini-count">
-							<span>0</span> <!-- 답변 -->
-						</div>
-						<div>answers</div>
-					</div>
-
 				</div>
 				<div class="question-list-left">
 					<h3 class="title">
-						<a href="/view/question.php">title</a> <!-- 제목 -->
+						<a href="/view/notice.php">title</a> <!-- 제목 -->
 					</h3>
-					<div class= "tags"> <!-- 태그 -->
-						<a href="" class= "tag">tag1</a>
-						<a href="" class= "tag">tag2</a>
-					</div>
+					
 				</div>
 				<div class="question-list-right">
 					<a class="star-off" href="#"></a>
