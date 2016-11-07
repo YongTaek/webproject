1994e54e0e51c2d558b073b5bf9e986e64e601d9
오용택 <ka123ak1@gmail.com>
2016-11-07 16:18:29 +0900

    [수정] header css 수정 [추가] notice , question css  추가

--- view/notice.php
+++ view/notice.php
@@ -5,47 +5,58 @@
 	<meta charset="utf-8">
 	<link rel="shortcut icon" href="icon/SelabFavicon.png" type="image/png">
 	<link rel="stylesheet" href="../public/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" href="../public/css/header.css" type="text/css">
+	<link rel="stylesheet" href="../public/css/base.css" type="text/css">
+	<link rel="stylesheet" href="../public/css/notice.css" type="text/css">
 </head>
 <body>
 	<header role = "banner" class="banner-color">
 		<nav role="navigation" >
 			<div id="logo" class="pull-left"><a href="/"><img class="logo" src="selab_logo_S.png" /></a></div>
 			<ul id="menu" class="inline-list pull-left">
-				<li class="pull-left"><a href="/notice" class="menu-item" >NOTICE</a></li>
-				<li class="pull-left"><a href="/members" class="menu-item">QUESTION</a></li>
-				<li class="pull-left"><a href="/free" class="menu-item">FREE BOARD</a></li>
+				<li class="pull-left"><a href="/view/notice.php" class="menu-item" >NOTICE</a></li>
+				<li class="pull-left"><a href="/view/question.php" class="menu-item">QUESTION</a></li>
+				<li class="pull-left"><a href="/view/" class="menu-item">FREE BOARD</a></li>
 			</ul>
 			<div role="login" class="pull-right">
 				<a id="login" href="/login">LOGIN</a>
 			</div>
 		</nav>
 		<div class = "jumbotron banner-color">
-			<h1 class="align-center">Home</h1>
+			<h1 class="align-center">Notice</h1>
 			<p class="lead align-center">Wed 3:30 ~ & Thu 10:30 ~ </p>
 		</div>
 	</header>
 
-	<div>
+	<div class="container">
 		<div class="notice">
 			<div class="title">
-				<h1>
-					<img src="">
+				<a class="star-off" href="#" ></a>
+				<h1 id="title_id">
 					<span>제목</span>
 				</h1>
-				<div>
-					<p>Content</p>
+				<div class="notice_info">
+					<span>author</span>
+					<span>date</span>
 				</div>
 			</div>
-			<div class="comment">
+			<div class="content">
+				<p>Content</p>
+			</div>
+		</div>
+		<!-- comment iterative-->
+		<?php ?>
+		<div class="comment">
+				<hr>
 				<div>
 					<span>content</span>
 					<span>author</span>
 					<span>date</span>
 				</div>
-			</div>
+				<hr>
 		</div>
-		<div></div>
+		<?php ?>
+		<div>
+		</div>
 	</div>
 
 </body>
