412c3c50fd6f219182d0b508723ae9f485e48a6e
gwpark <pakgunwoo@gmail.com>
2016-11-09 21:41:33 +0900

    notice.php, noticelist.php, question.php, questionlist.php

---
+++ php/noticelist.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="../public/css/bootstrap.min.css" type="text/css">
+	<link rel="stylesheet" type="text/css" href="../public/css/noticelist.css">
+	<link rel="stylesheet" href="../public/css/base.css" type="text/css">
+
+	<meta charset="utf-8">
+	<title>공지 게시판</title>
+</head>
+<body>
+	<header role ="banner">
+		<nav role="navigation" class="banner-color">
+			<div id="logo" class="pull-left">
+				<a href="/php/main.php"><img class="logo" src="/public/img/selab_logo_S.png" /></a>
+			</div>
+			<ul id="menu" class="inline-list pull-left">
+				<li class="pull-left"><a href="/php/noticelist.php" class="active menu-item" >NOTICE</a></li>
+				<li class="pull-left"><a href="/php/questionlist.php" class="menu-item">QUESTION</a></li>
+				<li class="pull-left"><a href="/php/freelist.php" class="menu-item">FREE BOARD</a></li>
+			</ul>
+			<div role="login" class="pull-right">
+			<a id="login" href="/php/login.php" class='pull-right'>LOGIN</a>
+			</div>
+		</nav>
+	</header><!-- /header -->
+	<div class = "jumbotron banner-color">
+		<h1 class="align-center">NOTICE</h1>
+		<p class="lead align-center">Wed 3:30 ~ & Thu 10:30 ~ </p>
+	</div>
+	<div class= "content">
+		<div class="subheader">
+			<a type="button" class="createBtn btn btn-primary" href="/notice/create">Ask Question</a>
+			<h2>ALL NOTICE</h2>
+			<ul class="nav nav-tabs">
+				<li class="question-tab active"><a herf = "/recent">recent</a></li>
+				<li class="question-tab"><a href = "/recommend">recommend</a></li>
+			</ul>
+		</div>
+		<div class= "qlist-wapper">
+
+			<?php
+				$db = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
+				$rows = $db->query("SELECT n.id, title, time, name FROM notice n JOIN user u ON n.u_id = u.id");
+				foreach ($rows as $row) {
+			?>
+
+			<div class= "question">
+				<div class= "question-num-summary">
+					<div class= "question-number">
+						<div class= "mini-count">
+							<span><?= $row["id"] ?></span> <!-- 문제 번호 -->
+						</div>
+						<div>indexs</div>
+					</div>
+				</div>
+				<div class="question-list-left">
+					<h3 class="title">
+						<a href= <?= "/php/notice.php?id=".$row["id"] ?> ><?= $row["title"] ?></a> <!-- 제목 -->
+					</h3>
+					
+				</div>
+				<div class="question-list-right">
+					<a class="star-off" href="#"></a>
+					<div>
+						<h5 class="date"><?= $row["time"] ?></h5> <!-- 날짜 -->
+						<h5 class="name">by. <?= $row["name"] ?></h5> <!--작성자 -->
+					</div>
+				</div>
+			</div>
+
+			<?php } ?>
+
+		</div>
+	</div>
+</body>
+</html>
