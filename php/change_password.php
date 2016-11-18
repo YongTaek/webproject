0c17baa8eb985a76a53f4a5477a373499adb9715
gwpark <pakgunwoo@gmail.com>
2016-11-18 14:19:54 +0900

    change_password.php

--- php/change_password.php
+++ php/change_password.php
@@ -6,12 +6,18 @@
     $new_pass = $_POST["password"];
 
     $db = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $rows = $db->query("SELECT * FROM user WHERE id = ".$_SESSION["id"]);
     if ($rows->rowCount() > 0) {
       $row = $rows->fetch();
       if (password_verify($cur_pass, $row["passwd"])) {
         $new_pass = password_hash($new_pass, PASSWORD_DEFAULT, 10);
-        $db->query("UPDATE user SET passwd = $new_pass WHERE id = ".$_SESSION["id"]);
+        try {
+          $db->query("UPDATE user SET passwd = '$new_pass' WHERE id = ".$_SESSION["id"]);
+        } catch (PDOException $ex) {
+          echo "Change Failed";
+          exit;
+        }
 
         header("Location: main.php");
         exit;
