d5b284a605d51a0706971afd8895425a59f334dc
gwpark <pakgunwoo@gmail.com>
2016-11-18 13:30:12 +0900

    logout?

--- php/main.php
+++ php/main.php
@@ -23,6 +23,8 @@
 			<div role="login" class="pull-right">
 				<div class="pull-right vr"></div>
 				<?php if (isset($_SESSION["id"]) && isset($_SESSION["name"]) && isset($_SESSION["auth"])) { ?>
+					<a id="login" href="login.php" class='pull-right'>LOGIN</a>
+					<div class="pull-right vr"></div>
 					<a id="mypage" href="#" class='pull-right'><?= $_SESSION["name"] ?> (<?= $_SESSION["auth"] ?>)</a>
 				<?php } else { ?>
 					<a id="login" href="dologin.php" class='pull-right'>LOGIN</a>
