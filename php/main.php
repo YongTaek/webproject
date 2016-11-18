9aab6b0fc326dcb4ec7e3680bcb88880f77bf319
gwpark <pakgunwoo@gmail.com>
2016-11-18 13:31:33 +0900

    commitcoccoccco

--- php/main.php
+++ php/main.php
@@ -26,12 +26,12 @@
 					<a id="login" href="login.php" class='pull-right'>LOGIN</a>
 					<div class="pull-right vr"></div>
 					<a id="mypage" href="#" class='pull-right'><?= $_SESSION["name"] ?> (<?= $_SESSION["auth"] ?>)</a>
+					<ul class="hidden" id="setting">
+					<li><a href="user-setting.php">Setting</a></li>
+					</ul>
 				<?php } else { ?>
 					<a id="login" href="dologin.php" class='pull-right'>LOGIN</a>
 				<?php } ?>
-				<ul class="hidden" id="setting">
-					<li><a href="user-setting.php">Setting</a></li>
-				</ul>
 			</div>
 			<img src="/public/img/search.png" class="pull-right search-icon">
 			<input type="text" class="pull-right search" name="search">
