7c090b44ce48360f392d4c471a0db197333e1afa
gwpark <pakgunwoo@gmail.com>
2016-11-25 14:07:07 +0900

    create, modify

--- php/modify_notice.php
+++ php/modify_notice.php
@@ -16,16 +16,23 @@
   <body>
     <header role = "banner" class="banner-color">
   		<nav role="navigation" >
-  			<div id="logo" class="pull-left"><a href="/view/main.php"><img class="logo" src="/public/img/selab_logo_S.png" /></a></div>
+  			<div id="logo" class="pull-left"><a href="/php/main.php"><img class="logo" src="/public/img/selab_logo_S.png" /></a></div>
   			<ul id="menu" class="inline-list pull-left">
-  				<li class="pull-left"><a href="/view/noticelist.php" class="menu-item active" >NOTICE</a></li>
-  				<li class="pull-left"><a href="/view/questionlist.php" class="menu-item">QUESTION</a></li>
-  				<li class="pull-left"><a href="/view/freelist.php" class="menu-item">FREE BOARD</a></li>
+  				<li class="pull-left"><a href="/php/noticelist.php" class="menu-item active" >NOTICE</a></li>
+  				<li class="pull-left"><a href="/php/questionlist.php" class="menu-item">QUESTION</a></li>
+  				<li class="pull-left"><a href="/php/freelist.php" class="menu-item">FREE BOARD</a></li>
   			</ul>
   			<div role="login" class="pull-right">
-  				<a id="login" href="/view/login.php" class='pull-right'>LOGIN</a>
-  				<div class="pull-right vr"></div>
-  				<a id="mypage" href="/view/myPage.php" class='pull-right'>천유정 (학생)</a>
+  				<?php if (isset($_SESSION["id"]) && isset($_SESSION["name"]) && isset($_SESSION["auth"])) { ?>
+            <a id="login" href="logout.php" class='pull-right'>LOGOUT</a>
+            <div class="pull-right vr"></div>
+            <a id="mypage" href="#" class='pull-right'><?= $_SESSION["name"] ?> (<?= $_SESSION["auth"] ?>)</a>
+            <ul class="hidden" id="setting">
+              <li><a href="user-setting.php">Setting</a></li>
+            </ul>
+          <?php } else { ?>
+            <a id="login" href="dologin.php" class='pull-right'>LOGIN</a>
+          <?php } ?>
   			</div>
   			<img src="/public/img/search.png" class="pull-right search-icon">
   			<input type="text" class="pull-right search" name="search">
