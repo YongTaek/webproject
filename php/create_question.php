548c853adda09b376a48948e133286c25fcb8fe2
오용택 <ka123ak1@gmail.com>
2016-11-28 18:23:40 +0900

    no message

--- php/create_question.php
+++ php/create_question.php
@@ -16,10 +16,10 @@
     $db->query("INSERT INTO question(u_id, title, content, time) VALUES($id, '$title', '$content', '$time')");
     $questionId = $db->query("SELECT id from question where title='$title' and content='$content' and time='$time' ");
     foreach ($tags as $tag) {
-      $tagId = $db->query("SELECT id from tag where name='$tag'");
-      if(!isset(tagId['id'])) {
+      $tagId = $db->query("SELECT id from tag where name=\"$tag\"");
+      if(!isset($tagId['id'])) {
         $db->query("INSERT INTO tag(name) values($tag)");
-        $tagId = $db->query("SELECT id from tag where name='$tag'");
+        $tagId = $db->query("SELECT id from tag where name=\"$tag\"");
       }
       $db->query("INSERT INTO tag_question(t_id, q_id) values($tagId,$questionId)");
     }
