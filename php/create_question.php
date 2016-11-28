de88d8202beaa1a367dc81ec538d818905b97211
지영 <skawl33@naver.com>
2016-11-28 18:53:13 +0900

    no message

--- php/create_question.php
+++ php/create_question.php
@@ -15,15 +15,15 @@
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->query("INSERT INTO question(u_id, title, content, time) VALUES($id, '$title', '$content', '$time')");
     $questionId = $db->query("SELECT id from question where title='$title' and content='$content' and time='$time'");
-    // foreach ($tags as $tag) {
-    //   $tagId = $db->query("SELECT id from tag where name='$tag'");
-    //   if(!isset($tagId['id'])) {
-    //     $db->query("INSERT INTO tag(name) values('$tag')");
-    //     $tagId = $db->query("SELECT id from tag where name='$tag'");
-    //   }
-    //   $db->query("INSERT INTO tag_question(t_id, q_id) values($tagId,$questionId)");
-    // }
-    header("Location: question.php?id=$questionId");
+    foreach ($tags as $tag) {
+      $tagId = $db->query("SELECT id from tag where name='$tag'");
+      if(!isset($tagId['id'])) {
+        $db->query("INSERT INTO tag(name) values('$tag')");
+        $tagId = $db->query("SELECT id from tag where name='$tag'");
+      }
+      $db->query("INSERT INTO tag_question(t_id, q_id) values($tagId,$questionId['id'])");
+    }
+    header("Location: question.php?id=$questionId['id']");
   } catch (PDOException $e) {
     echo $tag;
     echo $e->getMessage();
