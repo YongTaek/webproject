6749b7212d9c93e5dcee9276a41129a7b7ac5ea0
지영 <skawl33@naver.com>
2016-11-28 18:24:32 +0900

    no message

--- php/create_question.php
+++ php/create_question.php
@@ -7,23 +7,24 @@
   $content = $_POST["content"];
   $tag = $_POST["tags"];
   $time = date("Y-m-d H:i:s");
-
+  print $tag;
   $tags = explode($tag, ",");
-
+  print $tags;
   try {
     $db = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->query("INSERT INTO question(u_id, title, content, time) VALUES($id, '$title', '$content', '$time')");
-    $questionId = $db->query("SELECT id from question where title='$title' and content='$content' and time='$time' ");
+    $questionId = $db->query("SELECT id from question where title='$title' and content='$content' and time='$time'");
     foreach ($tags as $tag) {
-      $tagId = $db->query("SELECT id from tag where name='$tag'");
-      if(!isset(tagId['id'])) {
-        $db->query("INSERT INTO tag(name) values($tag)");
-        $tagId = $db->query("SELECT id from tag where name='$tag'");
+      $tagId = $db->query("SELECT id from tag where name=\"$tag\"");
+      if(!isset($tagId['id'])) {
+        $db->query("INSERT INTO tag(name) values('$tag')");
+        $tagId = $db->query("SELECT id from tag where name=\"$tag\"");
       }
       $db->query("INSERT INTO tag_question(t_id, q_id) values($tagId,$questionId)");
     }
   } catch (PDOException $e) {
+    echo $tag;
     echo $e->getMessage();
   }
 ?>
