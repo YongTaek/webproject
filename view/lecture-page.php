39aed231913fd86b2cdc1ff1b56bdf55299fce8e
ChoiSunYeong <vmffkxlgnqh1@gmail.com>
2016-11-28 18:03:24 +0900

    pdf view page

---
+++ view/lecture-page.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="../public/css/lecture-page.css" type="text/css">
+</head>
+<body>
+
+<embed src = "../files/Notes03(file_io)_3.pdf"/* file path */></embed>
+
+</body>
+</html>
