a2fcd237dec9518931c148012cf1f9b4ca3f929b
오용택 <ka123ak1@gmail.com>
2016-11-28 19:16:41 +0900

    no message

--- php/upload.php
+++ php/upload.php
@@ -1,36 +1,35 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
 <?php
 session_start();
-if(isset($_FILES['upload']['name']) && isset($_POST['url'])) {
-	echo "{ \"error\" : \"success\" }";
-} else if(isset($_FILES['upload']['name'])) {
-	$fileUrl = $uploaddir . basename($_FILES['upload']['name']);
-} else if(isset($_POST['url'])) {
-	$url = $_POST['url'];
+$authority = $_SESSION['auth'];
+if ($authority !== 'professor') {
+	$result = array("error" => "true");
+}
+if(!isset($result)) {
+	if(isset($_FILES['upload']['name']) && isset($_POST['url'])) {
+		$result = array("error" => "true");
+	} else if(isset($_FILES['upload']['name'])) {
+		$uploaddir = "../files/";
+		$fileUrl = $uploaddir . basename($_FILES['upload']['name']);
+		if(move_uploaded_file($_FILES['upload']['tmp_name'],$fileUrl)){
+			$dbUrl = $fileUrl;
+		} else{
+			$result = array("error" => "true");
+		}
+	} else if(isset($_POST['url'])) {
+		$dbUrl = $_POST['url'];
+	} else {
+		$result = array("error" => "true");
+	}
+	if(isset($result)) {
+		echo json_encode($result);
+	} else {
+		$name = $_POST["name"];
+		$db = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
+		$db->query("INSERT INTO lecture(name, url) values ('$name', '$dbUrl')");
+		$result = array("error" => "false");
+		echo json_encode($result);
+	}
 } else {
-	echo 
+		echo json_encode($result);
 }
-// 업로드한 파일이 저장될 디렉토리 정의
-$uploaddir = "../files/";  // 서버에 up 이라는 디렉토리가 있어야 한다.
-
-$fileUrl = $uploaddir . basename($_FILES['upload']['name']);
-if(move_uploaded_file($_FILES['upload']['tmp_name'],$fileUrl)){
-  echo "파일이 유효하고, 성공적으로 업로드 되었습니다.\n";
-  echo $fileUrl . "ha";
-} else{
-  echo $fileUrl;
-}
-$id = $_SESSION["id"];
-$name = $_POST["name"];
-$webUrl = $_POST["url"];
-
-$db = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
-$db->query("INSERT INTO lecture(name, url)")
 ?>
-</body>
-</html>
