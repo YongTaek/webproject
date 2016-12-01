d3b4bf6aa1220df6a839abc0d679936537509238
오용택 <ka123ak1@gmail.com>
2016-12-01 21:51:07 +0900

    no message

--- public/js/lecture-upload.js
+++ public/js/lecture-upload.js
@@ -1,30 +1,33 @@
 
 $(document).ready(function(){
-    var tabcontent = $(".tab");
-    $(".tab.url").css("display","none");
-    var i,tablinks;
-    $(".question-tab").click(function(){
-        tablinks = $(".question-tab");
-        tabcontent.css("display","none");
-        tablinks.removeClass("active");
-        $(this).addClass("active");
-        if($(this).hasClass('url')){
-            $(".url").css("display","block");
-            $(".url input").val('');
-        }
-        else{
-            $(".file").css("display","block");
-            $(".file input").val('');
-        }
-    });
-});
-$("#sub-mit").click(function () {
+  var tabcontent = $(".tab");
+  $(".tab.url").css("display","none");
+  var i,tablinks;
+  $(".question-tab").click(function(){
+    tablinks = $(".question-tab");
+    tabcontent.css("display","none");
+    tablinks.removeClass("active");
+    $(this).addClass("active");
+    if($(this).hasClass('url')){
+      $(".url").css("display","block");
+      $(".url input").val('');
+    }
+    else{
+      $(".file").css("display","block");
+      $(".file input").val('');
+    }
+  });
+  $("#sub-mit").click(function () {
     var options = {
-        dataType:"text",
-        success: function(responseText){
-            alert("업로드 성공!!");
-        },error: function(e){e.responseText();}
+      dataType:"text",
+      success: function(responseText){
+        alert("업로드 성공!!");
+      },error: function(e){
+        console.log(e.responseText());
+      }
     };
-
+    console.log('test');
     $("#form").ajaxForm(options).submit();
+  });
+
 });
