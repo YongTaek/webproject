d24fe215b9d6fd432c5a54d692b588638f10719d
오용택 <ka123ak1@gmail.com>
2016-12-08 16:06:10 +0900

    no message

--- php/create_comment.php
+++ php/create_comment.php
@@ -4,8 +4,9 @@
   date_default_timezone_set('Asia/Seoul');
 
   $r_id = $_POST["id"];
+  //$_SESSION["id"]
   $u_id = 2010000000;
-  $content = $_POST["comment"];
+  $content = $_POST["content"];
   $time = date("Y-m-d H:i:s");
   $type = $_POST["type"];
 
