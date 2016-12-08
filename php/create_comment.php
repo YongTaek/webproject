3d991f22df4ee43a9b545981bb6237134f20bcf6
오용택 <ka123ak1@gmail.com>
2016-12-08 23:25:48 +0900

    no message

--- php/create_comment.php
+++ php/create_comment.php
@@ -16,7 +16,7 @@
     $db = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->query("INSERT INTO comment(u_id, reference_id, content, time, type) VALUES($u_id, $r_id, '$content', '$time', '$type')");
-    $result = array("error" => "false", "content" => $conent, "time" => $time, "name" => $name );
+    $result = array("error" => "false", "content" => $content, "time" => $time, "name" => $name );
     // $pusher->trigger('lecture_channel', 'new_comment', $result);
 
   } catch (PDOException $e) {
