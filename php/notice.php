25b24199f9fa7234f4d85bed467331e84f58ee5b
오용택 <ka123ak1@gmail.com>
2016-12-08 19:14:30 +0900

    no message

--- php/notice.php
+++ php/notice.php
@@ -107,7 +107,7 @@
 				<label>Comment:</label>
 				<div>
 					<input id="comment-write" type="text" name="comment" />
-					<input class="btn" id="submit" type="submit" value="등록"/>
+					<input class="btn" id="submit" type="button" value="등록"/>
 				</div>
 				<input type="hidden" name="id" value="<?= $row["id"] ?>" />
 				<input type="hidden" name="type" value="notice">
