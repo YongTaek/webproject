189a7a9bfdf239b044c4a54d9dba7ff7431fee9a
지영 <skawl33@naver.com>
2016-12-08 18:14:23 +0900

    태그새끼

--- php/submit_question_modify.php
+++ php/submit_question_modify.php
@@ -7,33 +7,33 @@
   	$content = $_POST["content"];
   	$t = $_POST["tags"];
   	$tags = explode(",", $t);
-  	sort($tags);
-  	$origin_tag = $db->query("SELECT name, id FROM tag JOIN tag_question on t_id = id WHERE q_id = $id ORDER BY name");
+    $c_count = count($tags);
   	$i=0;
     try{
-        foreach ($origin_tag as $tag){
-            if($tag["name"] == $tags[$i]);
-            else{
-                $db->query("DELETE FROM tag_question WHERE q_id = $id");
-                $find->query("SELECT id FROM tag WHERE ".$tags[$i]." = name");
-                $found = $find->fetch();
-                $num = $find->rowCount();
-                if($num > 0){
-                     $db->query("INSERT INTO tag_question(t_id, q_id) values(".$found["id"].",$id)");
+        if($tags[0] == NULL){
+            $db->query("DELETE FROM tag_question WHERE q_id = $id");
+        }
+        else{
+            $db->query("DELETE FROM tag_question WHERE q_id = $id");
+            for($i=0;$i<$c_count;$i++){
+                $find = $db->query("SELECT id FROM tag WHERE name = ".$tags[$i]);
+                if(empty($find)){
+                    $db->query("INSERT INTO tag(name) values('$tags[$i]')"); 
+                    $newtag = $db->query("SELECT id FROM tag WHERE name = '$tags[$i]'");
+                    $tid = $newtag->fetch();
+                    $db->query("INSERT INTO tag_question(t_id, q_id) values(".$tid["id"].", $id)");
                 }
                 else{
-                    $db->query("INSERT INTO tag(name) values(tags[$i])");
-                    $addt = $db->query("SELECT id FROM tag WHERE name = tags[$i]");
-                    $tid = $addt->fetch();
-                    $db->query("INSERT INTO tag_question(t_id, q_id) values($tid, $id)");
+                    $db->query("INSERT INTO tag_question(t_id, q_id) values(".$find["id"].", $id)");
                 }
-            }
-            $i++; 
+            }   
         }
         $db->query("UPDATE question SET title = '$title' WHERE id = $id AND u_id = $u_id");
         $db->query("UPDATE question SET content = '$content' WHERE id = $id AND u_id = $u_id");
         header("Location: question.php?id=$id");
+
     } catch(PDOException $e){
+        echo $find;
         echo $e -> getMessage();
     }
 ?>
