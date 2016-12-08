5d02f570ee2a41f82fecb5293daef050ab51921b
오용택 <ka123ak1@gmail.com>
2016-12-08 15:37:07 +0900

    no message

--- php/create_comment.php
+++ php/create_comment.php
@@ -14,15 +14,9 @@
     $db = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->query("INSERT INTO comment(u_id, reference_id, content, time, type) VALUES($u_id, $r_id, '$content', '$time', '$type')");
-    // if ($type == "board")
-    //   header("Location: free.php?id=$r_id");
-    // elseif ($type == "answer")
-    //   header("Location: question.php?id=$r_id");
-    // else
-    //   header("Location: $type.php?id=$r_id");
     $result = array("error" => "false");
   } catch (PDOException $e) {
-    // echo $e->getMessage();
+    echo $e->getMessage();
     $result = array("error" => "true");
   }
   print json_encode($result);
