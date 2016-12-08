416c156bf16caa621efe6b8c780dc752fdcd872f
오용택 <ka123ak1@gmail.com>
2016-12-08 17:49:38 +0900

    no message

--- php/lecture-thread.php
+++ php/lecture-thread.php
@@ -1,7 +1,7 @@
 <div id="sidebar">
   <div class="threads">
     <?php
-      $comments = $db->query("SELECT c.id, c.content, c.time u.name from comment c join lecture l on c.type='lecture' and l.id=c.reference_id, join user u on u.id=c.u_id order by time limit 10");
+      $comments = $db->query("SELECT c.id, c.content, c.time u.name from comment c join lecture l on c.type=\"lecture\" and l.id=c.reference_id join user u on u.id=c.u_id order by time limit 10");
       foreach ($comments as $comment ) {
           $userName = $comment['name'];
           $time = $comment['time'];
