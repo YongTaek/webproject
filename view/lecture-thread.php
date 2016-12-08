be8c3590031e1fd960832be9deacb9e7f2fe9a46
Cheon Yu Jung <chyjis@naver.com>
2016-12-08 19:16:22 +0900

    sidebar design 수정

--- view/lecture-thread.php
+++ view/lecture-thread.php
@@ -1,10 +1,32 @@
 <div id="sidebar">
   <div class="threads">
     <div class="thread">
-      feejejek
+      <span class="content">궁금한게 있습니다</span>
+      <br>
+      <span class="writer">익명</span>
+      <br>
+      <span class="date">2016.12.08 6:45pm</span>
     </div>
     <div class="thread">
-      feejejek
+      <span class="content">궁금한게 있습니다</span>
+      <br>
+      <span class="writer">익명</span>
+      <br>
+      <span class="date">2016.12.08 6:45pm</span>
+    </div>
+    <div class="thread">
+      <span class="content">궁금한게 있습니다</span>
+      <br>
+      <span class="writer">익명</span>
+      <br>
+      <span class="date">2016.12.08 6:45pm</span>
+    </div>
+    <div class="thread">
+      <span class="content">궁금한게 있습니다</span>
+      <br>
+      <span class="writer">익명</span>
+      <br>
+      <span class="date">2016.12.08 6:45pm</span>
     </div>
   </div>
   <form class="lecture" action="create-comment.php" method="post">
