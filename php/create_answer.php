beb644c47ed945647d0e44fed0645723ce23f15a
지영 <skawl33@naver.com>
2016-12-08 18:48:59 +0900

    no message

--- php/create_answer.php
+++ php/create_answer.php
@@ -9,7 +9,7 @@
   	try{
   		$db = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
   		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  		$db->query("INSERT INTO answer(u_id, q_id, content, time, score) VALUES($u_id, $id, '$content', 'time', 0)");
+  		$db->query("INSERT INTO answer(u_id, q_id, content, time) VALUES($u_id, $id, '$content', '$time')");
   		header("Location: question.php?id=$id");
   	}catch(PDOException $e){
   		echo $e->getMessage();
