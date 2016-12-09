e016c273a4d4eb4bc07900d5e574c8534cd60745
Cheon Yu Jung <chyjis@naver.com>
2016-12-09 21:13:58 +0900

    no message

--- php/setting.php
+++ php/setting.php
@@ -8,6 +8,8 @@
   <link rel="stylesheet" href="/public/css/base.css" type="text/css">
   <script src="/public/js/jquery-3.1.1.min.js" type="text/javascript"></script>
   <link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet" type="text/css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
   <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
   <script src="//js.pusher.com/3.2/pusher.min.js"></script>
   <script src="/public/js/push.js"></script>
@@ -81,7 +83,7 @@
     <div id = "setting-student">
       <div class = "top-text">
         <h3 class="setting-text">Setting Student</h3>
-        <a id="add-student" href="#">파일로 학생 추가</a>
+        <button data-toggle="modal" data-target="#addStudent" id="add-student">파일로 학생 추가</button>
       </div>
       <table class="table table-striped">
         <thead>
@@ -105,7 +107,7 @@
     <div id="setting-assistant">
       <div class = "top-text">
         <h3 class="setting-text">Setting Assistant</h3>
-        <a id="add-assistant" href="">조교 추가</a>
+        <button data-toggle="modal" data-target="#squarespaceModal" id="add-assistant" href="">조교 추가</button>
       </div>
       <table class="table table-striped">
         <thead>
@@ -128,6 +130,85 @@
       <h3 class="setting-text">Change Password</h3>
       <a id="change-password" href="/php/changepw.php">비밀번호 변경</a>
     </div>
+
+    <!-- assistant modal -->
+    <div class="modal fade" id="squarespaceModal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+            <h3 class="modal-title" id="lineModalLabel">조교 추가</h3>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Assistant ID</label>
+                <input type="text" class="form-control" id="dialogAssID" placeholder="Enter Assistant ID...">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Assistant Name</label>
+                <input type="text" class="form-control" id="dialogAssName" placeholder="Enter Assistant Name...">
+              </div>
+            </form>
+
+          </div>
+          <div class="modal-footer">
+            <div class="btn-group btn-group-justified" role="group" aria-label="group button">
+              <div class="btn-group" role="group">
+                <button type="button" class="btn btn-default" data-dismiss="modal"  role="button">Close</button>
+              </div>
+              <div class="btn-group btn-delete hidden" role="group">
+                <button type="button" id="delImage" class="btn btn-default btn-hover-red" data-dismiss="modal"  role="button">Delete</button>
+              </div>
+              <div class="btn-group" role="group">
+                <button type="button" id="saveImage" class="btn btn-default btn-hover-green" data-action="save" role="button">Save</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal fade" id="addStudent" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+            <h3 class="modal-title" id="lineModalLabel">학생 추가</h3>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Student ID</label>
+                <input type="text" class="form-control" id="dialogStuID" placeholder="Enter Assistant ID...">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Student Name</label>
+                <input type="text" class="form-control" id="dialogStuName" placeholder="Enter Assistant Name...">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputFile">File input</label>
+                <input type="file" id="exampleInputFile">
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <div class="btn-group btn-group-justified" role="group" aria-label="group button">
+              <div class="btn-group" role="group">
+                <button type="button" class="btn btn-default" data-dismiss="modal"  role="button">Close</button>
+              </div>
+              <div class="btn-group btn-delete hidden" role="group">
+                <button type="button" id="delImage" class="btn btn-default btn-hover-red" data-dismiss="modal"  role="button">Delete</button>
+              </div>
+              <div class="btn-group" role="group">
+                <button type="button" id="saveImage" class="btn btn-default btn-hover-green" data-action="save" role="button">Save</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </body>
 </html>
