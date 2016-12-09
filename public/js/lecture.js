8fc50e064c3a232e5580e759918d8eb4dc40ce87
ChoiSunYeong <vmffkxlgnqh1@gmail.com>
2016-12-10 00:12:06 +0900

    no message

--- public/js/lecture.js
+++ public/js/lecture.js
@@ -36,7 +36,7 @@ function changeDrawerClass(event) {
     $(event.target).removeClass('opendrawer');
     $(event.target).addClass('closedrawer');
   } else {
-    $(".threads").animate({scrollTop: $(".threads").prop("scrollHeight")});
+    $(".threads").scrollTop($(".threads").prop("scrollHeight"));
     $(event.target).addClass('opendrawer');
     $(event.target).removeClass('closedrawer');
   };
