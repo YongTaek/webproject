0f6f9bfd7d29948530c6b6b4ba31d5bbfe6f51bc
Cheon Yu Jung <chyjis@naver.com>
2016-12-09 23:55:58 +0900

    no message

--- public/js/settingDialog.js
+++ public/js/settingDialog.js
@@ -1,6 +1,16 @@
 
-$("a.answer_modify").on('click', function () {
-	
+$("#deleteDiagramStudent").on('click', function () {
+	console.log("deleteDiagramStudent");
 }
 
+$("#saveDiagramStudent").on('click', function () {
+	console.log("saveDiagramStudent");
+}
 
+$("#deleteDiagramAssistant").on('click', function () {
+	console.log("deleteDiagramAssistant");
+}
+
+$("#saveDiagramAssistant").on('click', function () {
+	console.log("saveDiagramAssistant");
+}
