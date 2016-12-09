c01c9a6d64a097bf34eba5ac5aaeb9541693e8c5
오용택 <ka123ak1@gmail.com>
2016-12-09 23:04:44 +0900

    no message

--- public/js/question.js
+++ public/js/question.js
@@ -21,39 +21,39 @@ function questionReady(){
 };
 
 function redirectComment(da,comment){
-	// console.log(comment);
-	// var content = da.content;
-	// // var content = "hello";
-	// var time = da.time;
-	// // var time = "2016.12.08 6:45pm";
-	// var name = da.name;
-	// // var name = "익명";
+	console.log(comment);
+	var content = da.content;
+	// var content = "hello";
+	var time = da.time;
+	// var time = "2016.12.08 6:45pm";
+	var name = da.name;
+	// var name = "익명";
 
-	// var div = $("<div></div>");
+	var div = $("<div></div>");
 
-	// var spancontent = $("<span></span>").text(content);
-	// var spanname = $("<span></span>").text(name);
-	// var spantime = $("<span></span>").text(time);
+	var spancontent = $("<span></span>").text(content);
+	var spanname = $("<span></span>").text(name);
+	var spantime = $("<span></span>").text(time);
 
-	// var btns = $("<div class = 'comment_btn'></div>");
+	var btns = $("<div class = 'comment_btn'></div>");
 
-	// var edit = $("<a class = 'btn question_modify' name='question_modify'>수정</a>");
-	// edit.href = "modify_question.php?id=" + da.r_id;
+	var edit = $("<a class = 'btn question_modify' name='question_modify'>수정</a>");
+	edit.href = "modify_question.php?id=" + da.r_id;
 
-	// var remove = $("<a class = 'btn question_delete' name = 'question_delete'>삭제</a>");
-	// remove.href = "delete_question.php?id=" + da.r_id;
+	var remove = $("<a class = 'btn question_delete' name = 'question_delete'>삭제</a>");
+	remove.href = "delete_question.php?id=" + da.r_id;
 
-	// btns.append(edit);
-	// btns.append(remove);
+	btns.append(edit);
+	btns.append(remove);
 
-	// div.append(spancontent," ");
-	// div.append(spanname," ");
-	// div.append(spantime," ");
-	// div.append(btns);
+	div.append(spancontent," ");
+	div.append(spanname," ");
+	div.append(spantime," ");
+	div.append(btns);
 
-	// $(comment).append(div);
-	// $(comment).append($("<hr>"));
-	window.location.href = document.location.href;
+	$(comment).append(div);
+	$(comment).append($("<hr>"));
+	// window.location.href = document.location.href;
 
 };
 
