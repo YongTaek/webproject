7b54d8dfaff2555b5c6ee04d3db99be28f8b9a7b
gwpark <pakgunwoo@gmail.com>
2016-12-09 23:32:59 +0900

    vote...

--- php/vote.php
+++ php/vote.php
@@ -11,7 +11,7 @@
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     if ($score == "up") {
-      $rows = $db->query("SELECT * FROM user_question WHERE u_id = $u_id AND r_id = $id AND type = $type");
+      $rows = $db->query("SELECT * FROM user_question WHERE u_id = $u_id AND r_id = $id AND type = '$type'");
       if ($rows->rowCount() > 0)
         throw new PDOException("Already Scored", 1);
       else {
@@ -19,7 +19,7 @@
         $db->query("UPDATE question SET score = score + 1 WHERE id = $id");
       }
     } else {
-      $rows = $db->query("SELECT * FROM user_question WHERE u_id = $u_id AND r_id = $id AND type = $type");
+      $rows = $db->query("SELECT * FROM user_question WHERE u_id = $u_id AND r_id = $id AND type = '$type'");
       if ($rows->rowCount() > 0)
         throw new PDOException("Already Scored", 1);
       else {
@@ -30,7 +30,6 @@
 
     $result = array("error" => "false");
   } catch (PDOException $e) {
-    echo $e->getMessage();
     $result = array("error" => "true");
   }
   header("Content-type: application/json");
