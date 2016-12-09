438e35cbba6037934ec3d7ce43bd2a72ed72d071
오용택 <ka123ak1@gmail.com>
2016-12-09 22:41:45 +0900

    no message

--- public/js/pusher.js
+++ public/js/pusher.js
@@ -6,78 +6,82 @@ var pusher = new Pusher('dc9f3fc01f0f63f45083', {
 	encrypted: true
 });
 for (var i = 0; i < questionArray.length; i++) {
-  var channel = pusher.subscribe(questionArray[i]);
-  channel.bind('new_comment', function(data) {
-  	// https://github.com/CodeSeven/toastr#escape-html-characters
-  	// http://codeseven.github.io/toastr/demo.html
-  	toastr.options = {
-  		"closeButton": true,
-  		"debug": false,
-  		"newestOnTop": false,
-  		"progressBar": false,
-      "onclick" : function () {
+	var channel = pusher.subscribe(questionArray[i]);
+	channel.bind('new_comment', function(data) {
+		// https://github.com/CodeSeven/toastr#escape-html-characters
+		// http://codeseven.github.io/toastr/demo.html
+		toastr.options = {
+			"closeButton": true,
+			"debug": false,
+			"newestOnTop": false,
+			"progressBar": false,
+			"onclick" : function () {
 				var link = document.location.href;
 				if (link === data.url) {
 					appendComment(data);
 				} else {
-        	// window.location.href = data.url;
+					// window.location.href = data.url;
 					alert('asfd');
 				}
-      },
-  		"positionClass": "toast-top-right",
-  		"preventDuplicates": true,
-  		"showDuration": "300",
-  		"hideDuration": "1000",
-  		"timeOut": "10000",
-  		"extendedTimeOut": "1000",
-  		"showEasing": "swing",
-  		"hideEasing": "linear",
-  		"showMethod": "fadeIn",
-  		"hideMethod": "fadeOut"
-  	};
+			},
+			"positionClass": "toast-top-right",
+			"preventDuplicates": true,
+			"showDuration": "300",
+			"hideDuration": "1000",
+			"timeOut": "10000",
+			"extendedTimeOut": "1000",
+			"showEasing": "swing",
+			"hideEasing": "linear",
+			"showMethod": "fadeIn",
+			"hideMethod": "fadeOut"
+		};
 
-  	//toastr.info(data.message,'질문 등록');
-  	// toastr.warning(data.message, '질문 경고');
-  	//toastr.success(data.message, '질문 등록 성공');
-  	// toastr.error(data, '질문 실패');
-    Command: toastr["info"](data.content);
-  	// 골라서 쓰기
-  });
+		//toastr.info(data.message,'질문 등록');
+		// toastr.warning(data.message, '질문 경고');
+		//toastr.success(data.message, '질문 등록 성공');
+		// toastr.error(data, '질문 실패');
+		Command: toastr["info"](data.content);
+		// 골라서 쓰기
+	});
 }
 for (var i = 0; i < lectureArray.length; i++) {
-  var channel = pusher.subscribe(lectureArray[i]);
-  channel.bind('new_comment', function(data) {
-  	// https://github.com/CodeSeven/toastr#escape-html-characters
-  	// http://codeseven.github.io/toastr/demo.html
-  	toastr.options = {
-  		"closeButton": true,
-  		"debug": false,
-  		"newestOnTop": false,
-  		"progressBar": false,
-      "onclick" : function () {
-				var link = document.location.href;
-				if (link === data.url) {
-        	// window.location.href = data.url;
-					alert('asfd');
-				}
-      },
-  		"positionClass": "toast-top-right",
-  		"preventDuplicates": true,
-  		"showDuration": "300",
-  		"hideDuration": "1000",
-  		"timeOut": "10000",
-  		"extendedTimeOut": "1000",
-  		"showEasing": "swing",
-  		"hideEasing": "linear",
-  		"showMethod": "fadeIn",
-  		"hideMethod": "fadeOut"
-  	};
+	var channel = pusher.subscribe(lectureArray[i]);
+	channel.bind('new_comment', function(data) {
+		// https://github.com/CodeSeven/toastr#escape-html-characters
+		// http://codeseven.github.io/toastr/demo.html
+		var link = document.location.href;
+		console.log("asdf");
+		if (link === data.url) {
+			// window.location.href = data.url;
+			appendComment(data);
+			alert('asfd');
+		} else {
+			toastr.options = {
+				"closeButton": true,
+				"debug": false,
+				"newestOnTop": false,
+				"progressBar": false,
+				"onclick" : function () {
+					window.location.href = data.url;
+				},
+				"positionClass": "toast-top-right",
+				"preventDuplicates": true,
+				"showDuration": "300",
+				"hideDuration": "1000",
+				"timeOut": "10000",
+				"extendedTimeOut": "1000",
+				"showEasing": "swing",
+				"hideEasing": "linear",
+				"showMethod": "fadeIn",
+				"hideMethod": "fadeOut"
+			};
 
-  	//toastr.info(data.message,'질문 등록');
-  	// toastr.warning(data.message, '질문 경고');
-  	//toastr.success(data.message, '질문 등록 성공');
-  	// toastr.error(data, '질문 실패');
-  	// 골라서 쓰기
-    Command: toastr["info"](data.content);
-  });
+			//toastr.info(data.message,'질문 등록');
+			// toastr.warning(data.message, '질문 경고');
+			//toastr.success(data.message, '질문 등록 성공');
+			// toastr.error(data, '질문 실패');
+			// 골라서 쓰기
+			Command: toastr["info"](data.content);
+		}
+	});
 }
