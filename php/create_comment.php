0a23ccc4ca829f4fffd9f4d0c9befde3bd087a31
오용택 <ka123ak1@gmail.com>
2016-12-09 15:53:31 +0900

    no message

--- php/create_comment.php
+++ php/create_comment.php
@@ -2,8 +2,8 @@
   session_start();
 
   date_default_timezone_set('Asia/Seoul');
-  // require('./library/Pusher.php');
-  // require('./library/push_setting.php');
+  require('./library/Pusher.php');
+  require('./library/push_setting.php');
 
   $r_id = $_POST["id"];
   $u_id = $_SESSION["id"];
@@ -15,11 +15,12 @@
   try {
     $db = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $db->query("INSERT INTO comment(u_id, reference_id, content, time, type) VALUES($u_id, $r_id, '$content', '$time', '$type')");
+    $db->query("INSERT INTO comment(u_id, reference_id, content, time, type) VALUES ($u_id, $r_id, '$content', '$time', '$type')");
     $result = array("error" => "false", "content" => $content, "time" => $time, "name" => $name );
     // $pusher->trigger('lecture_channel', 'new_comment', $result);
 
   } catch (PDOException $e) {
+    $e -> getMessage();
     $result = array("error" => "true", "r_id" => $r_id, "content" => $content, "type" => $type);
   }
   print json_encode($result);
