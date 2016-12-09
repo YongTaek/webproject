2b6e5613ebdf5db950e912d1647613a22cdd7b06
지영 <skawl33@naver.com>
2016-12-09 23:10:11 +0900

    no message

--- public/js/lec-open-close.js
+++ public/js/lec-open-close.js
@@ -2,20 +2,30 @@
 function func(){
 	$(".lecture-open").click(function (event) {
 
-		if ($(this).hasClass("lecture-close")){
-			$(this).text("Open");
-			$(this).removeClass("lecture-close");
+		if ($(this).hasClass("lecture-close")) {
 			var status = "open";
 		} else {
-			$(this).text("Close");
-			$(this).addClass("lecture-close");
 			var status = "close";
 		}
+
 		$.ajax({
 			url: "lec-open-close"
 			context: this,
 			data: {
 				id: status
+			}
+			success: function (data){
+				if(data.error === "false"){
+					if (status == "open") {
+						$(this).text("Open");
+						$(this).removeClass("lecture-close");
+						$(this).addClass("lecture-open");
+					} else {
+						$(this).text("Close");
+						$(this).removeClass("lecture-open");
+						$(this).addClass("lecture-close");
+					}
+				}
 			}
 		});
 	});
