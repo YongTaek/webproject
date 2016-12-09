238eaa817549cb7d9f6e69aecec1c3836082561b
지영 <skawl33@naver.com>
2016-12-10 01:03:56 +0900

    no message

--- php/upload.php
+++ php/upload.php
@@ -28,7 +28,7 @@
 			$name = $_POST["title"];
 			$db = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
 			if(isset($_POST["id"])){
-				$db->query("UPDATE lecture SET name = '$name', url = '$dbUrl' WHERE id = $_POST["id"]")
+				$db->query("UPDATE lecture SET name = '$name', url = '$dbUrl' WHERE id = $_POST["id"]");
 			}
 			else{
 				$db->query("INSERT INTO lecture(name, url) VALUES ('$name', '$dbUrl')");	
