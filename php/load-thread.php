37d76ef42038e6d9450dcad17b9764f42ae3d840
ChoiSunYeong <vmffkxlgnqh1@gmail.com>
2016-12-10 14:00:32 +0900

    asdf

--- php/load-thread.php
+++ php/load-thread.php
@@ -4,7 +4,7 @@
   $r_id = $_POST["id"];
   $time = $_POST["date"];
   // print $time;
-  $time = DateTime::createFromFormat('Y-m-d H:i:s', $time)->format('Y-m-d H:i:s');
+  $time = new DateTime($time);
   // $time = strtotime($time);
   // $time = date("Y-m-d H:i:s",$time);
   // print $time;
