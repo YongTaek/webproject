e46e21c412068faa11b3c0fdeaa3443a32280567
오용택 <ka123ak1@gmail.com>
2016-12-11 00:22:45 +0900

    no message

--- php/main.php
+++ php/main.php
@@ -15,20 +15,23 @@
 	<link rel="stylesheet" href="/public/css/main.css" type="text/css">
 	<link rel="stylesheet" href="/public/css/base.css" type="text/css">
 	<link rel="stylesheet" href="/public/css/sidebar.css" type="text/css">
-	<script type="text/javascript">
-		<?php if (isset($_SESSION["id"]) && isset($_SESSION["favQuestion"]) && isset($_SESSION["openLecture"])) { ?>
-			var questionArray = <?php echo json_encode($_SESSION["favQuestion"]); ?>;
-			var lectureArray = <?php echo json_encode($_SESSION["openLecture"]); ?>;
-		<?php } ?>
-	</script>
 	<script src="/public/js/jquery-3.1.1.min.js" type="text/javascript"></script>
 	<script src="/public/js/jquery-ui-1.12.1.min.js"></script>
+	<script src="/public/js/base.js"></script>
+	<script type="text/javascript">
+		<?php if (isset($_SESSION["id"]) && isset($_SESSION["favQuestion"]) && isset($_SESSION["openLecture"])) {
+			$userId = $_SESSION["id"];
+		?>
+		var questionArray = <?php echo json_encode($_SESSION["favQuestion"]); ?>;
+		var lectureArray = <?php echo json_encode($_SESSION["openLecture"]); ?>;
+		<?php } ?>
+	</script>
+
 	<link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet" type="text/css">
 	<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 	<script src="//js.pusher.com/3.2/pusher.min.js"></script>
 	<script src="/public/js/push.js"></script>
 	<script src="/public/js/pusher.js"></script>
-	<script src="/public/js/base.js"></script>
 </head>
 <body>
 	<header role = "banner" class="banner-color">
--- php/notification-side.php
+++ php/notification-side.php
@@ -1,8 +1,31 @@
+<?php
+  $db = new PDO("mysql:dbname=qna;host=localhost;charset=utf8", "root", "root");
+  $db->exec("set names utf8");
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $rows = $db->query("SELECT message, url, time from notification where u_id=$userId");
+  $count = 0;
+?>
+
 <div id="mySidenav" class="sidenav">
   <span class="text">Notification</span>
   <a href="javascript:void(0)" class="closebtn">&times;</a>
   <hr>
   <!-- <span class="no-notification">받지 않은 알림이 없습니다!</span> -->
   <div id="notifications">
+    <?php
+     foreach ($rows as $row) {
+        $message = $row["message"];
+        $url = $row["url"];
+        $time = $row["time"];
+        $count++;
+    ?>
+    <a class="notification" href="<?=$url?>">
+      <div class="notification-thread">
+        <p><?=$message?></p>
+        <p><?=$time?></p>
+      </div>
+    </a>
+    <?php
+      } ?>
   </div>
 </div>
