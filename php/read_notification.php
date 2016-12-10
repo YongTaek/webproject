d96f58f7ae43a60d9e159a67ebe7cb2fd381d824
오용택 <ka123ak1@gmail.com>
2016-12-10 19:39:14 +0900

    no message

--- php/read_notification.php
+++ php/read_notification.php
@@ -1,6 +1,9 @@
 <?php
   $data = json_decode($_POST["data"]);
-  $id = $data["id"];
+  $id = $_SESSION["id"];
+  if (isset($id)) {
+    header("Location: main.php?id=$id");
+  }
   $content = $data["content"];
   $time = $data["time"];
   $url = $data["url"];
@@ -10,7 +13,11 @@
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $db->query("UPDATE notification SET isread = 1 WHERE u_id = $id AND message = '$content' AND url = '$url' AND time = '$time'");
+    $array = array("error" => "false");
+    print json_encode($array);
   } catch (PDOException $e) {
-    
+    print $e;
+    $array = array("error" => "true");
+
   }
 ?>
