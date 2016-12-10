12450b1637547c5ea42273ac80f0c0d88fbad122
오용택 <ka123ak1@gmail.com>
2016-12-10 20:20:18 +0900

    no message

--- php/read_notification.php
+++ php/read_notification.php
@@ -15,7 +15,7 @@
     $db = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $db->query("UPDATE notification SET isread = 1 WHERE u_id=$id and message='$content' and url='$url' and time='$time'");
+    $db->query("UPDATE notification SET isread=1 WHERE u_id=$id and message='$content' and url='$url' and time='$time'");
     $array = array("error" => "false");
     print json_encode($array);
   } catch (PDOException $e) {
