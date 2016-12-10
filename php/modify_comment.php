edd974f69c02d98efbd07acf59196f4641e6d2df
오용택 <ka123ak1@gmail.com>
2016-12-10 17:10:58 +0900

    no message

--- php/modify_comment.php
+++ php/modify_comment.php
@@ -7,14 +7,15 @@
   $content = $_POST["content"];
   $time = date("Y-m-d H:i:s");
   $redirectId = $_POST["questionId"];
+  print $redirectId. "/";
   $db = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   try {
     $db->query("UPDATE comment set content=\"$content\" where id=$id");
     $db->query("UPDATE comment set time=\"$time\" where id=$id");
-    $array = array("error" => "false");
-    header("Location: question.php?id=$redirectId");
+    // $array = array("error" => "false");
+    // header("Location: question.php?id=$redirectId");
   } catch (Exception $e) {
     echo $e -> getMessage();
   }
