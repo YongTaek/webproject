f2c71038b4ec1582a380986c116646843df110d2
오용택 <ka123ak1@gmail.com>
2016-12-10 23:39:55 +0900

    no message

--- public/js/pusher.js
+++ public/js/pusher.js
@@ -24,6 +24,7 @@ for (var i = 0; i < questionArray.length; i++) {
 				a.remove();
 				$("#notification").text(parseInt($("#notification").text()) - 1);
 				sendReadMessage(data);
+				window.location.href = data.url;
 			},
 			"positionClass": "toast-top-right",
 			"preventDuplicates": true,
@@ -70,7 +71,6 @@ function sendReadMessage(notification) {
 		dataType: 'json',
 		success : function (result) {
 			console.log(result);
-			window.location.href = data.url;
 		},
 		error : function (result) {
 			console.log(result);
