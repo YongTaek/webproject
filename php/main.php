0faa143c75a27e4803fa12917c70e2e534ab5b1f
오용택 <ka123ak1@gmail.com>
2016-12-11 00:35:50 +0900

    no message

--- php/main.php
+++ php/main.php
@@ -3,7 +3,22 @@
 	$logged_in = false;
   if (isset($_SESSION["id"]) && isset($_SESSION["name"]) && isset($_SESSION["auth"])) {
     $logged_in = true;
+		$userId = $_SESSION["id"];
   }
+	$db = new PDO("mysql:dbname=qna;host=localhost;charset=utf8", "root", "root");
+	$db->exec("set names utf8");
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$rows = $db->query("SELECT message, url, time from notification where u_id=$userId and isread=0");
+	$count = 0;
+	$pushArray = array();
+	foreach ($rows as $row) {
+		$message = $row["message"];
+		$url = $row["url"];
+		$time = $row["time"];
+		$count++;
+		$tempArray = array("message" => $message, "url" => $url, "time" => $time);
+		$pushArrayp[] = $tempArray;
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -20,11 +35,11 @@
 	<script src="/public/js/base.js"></script>
 	<script type="text/javascript">
 		<?php if (isset($_SESSION["id"]) && isset($_SESSION["favQuestion"]) && isset($_SESSION["openLecture"])) {
-			$userId = $_SESSION["id"];
 		?>
 		var questionArray = <?php echo json_encode($_SESSION["favQuestion"]); ?>;
 		var lectureArray = <?php echo json_encode($_SESSION["openLecture"]); ?>;
 		<?php } ?>
+
 	</script>
 
 	<link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet" type="text/css">
--- php/notification-side.php
+++ php/notification-side.php
@@ -1,11 +1,3 @@
-<?php
-  $db = new PDO("mysql:dbname=qna;host=localhost;charset=utf8", "root", "root");
-  $db->exec("set names utf8");
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $rows = $db->query("SELECT message, url, time from notification where u_id=$userId and isread=0");
-  $count = 0;
-?>
-
 <div id="mySidenav" class="sidenav">
   <span class="text">Notification</span>
   <a href="javascript:void(0)" class="closebtn">&times;</a>
@@ -13,19 +5,16 @@
   <!-- <span class="no-notification">받지 않은 알림이 없습니다!</span> -->
   <div id="notifications">
     <?php
-     foreach ($rows as $row) {
-        $message = $row["message"];
-        $url = $row["url"];
-        $time = $row["time"];
-        $count++;
+    foreach ($pushArray as $array ) {
     ?>
-    <a class="notification" href="<?=$url?>">
+    <a class="notification" href="<?=$array["url"]?>">
       <div class="notification-thread">
-        <p><?=$message?></p>
-        <p><?=$time?></p>
+        <p><?=$array["message"]?></p>
+        <p><?=$array["time"]?></p>
       </div>
     </a>
     <?php
-      } ?>
+      }
+    ?>
   </div>
 </div>
