1364593f62fa3f608512741230bf0291ca9d6be1
지영 <skawl33@naver.com>
2016-12-10 14:44:17 +0900

    no message

--- php/submit_answer_modify.php
+++ php/submit_answer_modify.php
@@ -1,13 +1,9 @@
 <?php
 	session_start();
-
 	$db = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
 	$id = $_POST["id"];
   	$u_id = $_SESSION["id"];
   	$content = $_POST["content"];
-  	try{
-  		$db->query("UPDATE answer SET content = '$content' WHERE q_id = $id AND u_id = $u_id");
-    } catch(PDOException $e){
-        echo $e -> getMessage();
-    }
+  	$db->query("UPDATE answer SET content = '$content' WHERE q_id = $id AND u_id = $u_id");
+  	header("Location: question.php?id=$id");
 ?>
