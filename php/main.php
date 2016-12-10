0fb512057c133253f9efff3d432f0bfd23d33ca0
오용택 <ka123ak1@gmail.com>
2016-12-11 00:50:47 +0900

    no message

--- php/main.php
+++ php/main.php
@@ -4,21 +4,22 @@
   if (isset($_SESSION["id"]) && isset($_SESSION["name"]) && isset($_SESSION["auth"])) {
     $logged_in = true;
 		$userId = $_SESSION["id"];
+		$db = new PDO("mysql:dbname=qna;host=localhost;charset=utf8", "root", "root");
+		$db->exec("set names utf8");
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$rows = $db->query("SELECT message, url, time from notification where u_id=$userId and isread=0");
+		$count = 0;
+		$pushArray = array();
+		foreach ($rows as $row) {
+			$message = $row["message"];
+			$url = $row["url"];
+			$time = $row["time"];
+			$count++;
+			$tempArray = array("message" => $message, "url" => $url, "time" => $time);
+			$pushArray[] = $tempArray;
+		}
   }
-	$db = new PDO("mysql:dbname=qna;host=localhost;charset=utf8", "root", "root");
-	$db->exec("set names utf8");
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$rows = $db->query("SELECT message, url, time from notification where u_id=$userId and isread=0");
-	$count = 0;
-	$pushArray = array();
-	foreach ($rows as $row) {
-		$message = $row["message"];
-		$url = $row["url"];
-		$time = $row["time"];
-		$count++;
-		$tempArray = array("message" => $message, "url" => $url, "time" => $time);
-		$pushArray[] = $tempArray;
-	}
+
 ?>
 <!DOCTYPE html>
 <html>
