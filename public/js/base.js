aac7e74ffa4328f1a2ff226a327076e91ce3df83
Cheon Yu Jung <chyjis@naver.com>
2016-12-10 21:49:20 +0900

    side-bar 추가

--- public/js/base.js
+++ public/js/base.js
@@ -7,5 +7,21 @@ function onclick(){
 			alert("키워드를 입력해주세요 >:(");
 		}
 	});
+
+	$(".side-bar").click(function(){
+
+    	document.getElementById("mySidenav").style.width = "250px";
+
+	});
+	
+	$(".closebtn").click(function(){
+
+		 document.getElementById("mySidenav").style.width = "0";
+	});
+
 };
+
+
+
+
 $(document).ready(onclick);
