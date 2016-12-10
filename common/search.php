a959a0261ed512eba298aea34763ff043aec2e08
Cheon Yu Jung <chyjis@naver.com>
2016-12-11 03:58:56 +0900

    search page 수정

--- common/search.php
+++ common/search.php
@@ -31,7 +31,7 @@
 		</div>
 		<div class= "qlist-wapper">
 
-			<p class="text">Notice</p>
+			<p class="search-text">Notice</p>
 			<?php
 				$db = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
 				$rows = $db->query("SELECT n.id, title, time, name FROM notice n JOIN user u ON n.u_id = u.id WHERE title like \"%$keyword%\" ORDER BY pinned DESC, time DESC");
@@ -59,7 +59,7 @@
 				</div>
 			</div>
 			<?php } ?>
-			<p class="text">Question</p>
+			<p class="search-text">Question</p>
 			<?php
 				$rows = $db->query("SELECT distinct q.id, q.title, time, u.name FROM question q JOIN user u ON q.u_id = u.id JOIN tag t JOIN tag_question tq ON t.id = tq.t_id WHERE title like \"%$keyword%\" or t.name like \"%$keyword%\" ORDER BY time DESC");
 				foreach ($rows as $row) {
@@ -96,7 +96,7 @@
 				</div>
 			</div>
 			<?php } ?>
-			<p class="text">Board</p>
+			<p class="search-text">Board</p>
 			<?php
 				$rows = $db->query("SELECT b.id, title, time, name FROM board b JOIN user u ON b.u_id = u.id WHERE title like \"%$keyword%\" ORDER BY pinned DESC, time DESC");
 
@@ -124,7 +124,7 @@
 				</div>
 			</div>
 			<?php } ?>
-			<p class="text">Lecture</p>
+			<p class="search-text">Lecture</p>
 			<?php
 				$rows = $db->query("SELECT id, name FROM lecture WHERE name like \"%$keyword%\"");
 				foreach ($rows as $row) {
