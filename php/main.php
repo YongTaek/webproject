43ba05a982888a99e637f5b0f37d4f12513567ba
오용택 <ka123ak1@gmail.com>
2016-12-11 00:42:15 +0900

    no message

--- php/main.php
+++ php/main.php
@@ -5,10 +5,15 @@
     $logged_in = true;
 		$userId = $_SESSION["id"];
   }
+	print($userId);
 	$db = new PDO("mysql:dbname=qna;host=localhost;charset=utf8", "root", "root");
 	$db->exec("set names utf8");
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$rows = $db->query("SELECT message, url, time from notification where u_id=\"$userId\" and isread=0");
+	try{
+	$rows = $db->query("SELECT message, url, time from notification where u_id=$userId and isread=0");
+	} catch(PDOException $e) {
+		print $e->getMessage();
+	}
 	$count = 0;
 	$pushArray = array();
 	foreach ($rows as $row) {
