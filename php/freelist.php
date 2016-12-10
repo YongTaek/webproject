b38fba34495b85ad1898a17d5dc5f9b0645180fa
ChoiSunYeong <vmffkxlgnqh1@gmail.com>
2016-12-10 16:46:12 +0900

    연하남 건우

--- php/freelist.php
+++ php/freelist.php
@@ -56,8 +56,11 @@
 					<a id="login" href="dologin.php" class='pull-right'>LOGIN</a>
 				<?php } ?>
 			</div>
-			<a href="/view/free/search"><img src="/public/img/search.png" class="pull-right search-icon"></a>
+			<form>
+			<input type="image" src="/public/img/search.png" class="pull-right search-icon">
+			<!-- <a href="/view/question/search"><img src="/public/img/search.png" class="pull-right search-icon"></a> -->
 			<input type="text" class="pull-right search" name="search">
+			</form>
 		</nav>
 	</header><!-- /header -->
 	<div class = "jumbotron banner-color">
