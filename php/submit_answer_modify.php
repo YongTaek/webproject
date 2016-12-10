65bebfdfc69c60d324a2df0975108cd1308e97bc
지영 <skawl33@naver.com>
2016-12-10 14:47:22 +0900

    답변 수정 ㄹㅇㄹㅇㄹ끗

--- php/submit_answer_modify.php
+++ php/submit_answer_modify.php
@@ -1,9 +1,14 @@
 <?php
 	session_start();
+
 	$db = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
 	$id = $_POST["id"];
   	$u_id = $_SESSION["id"];
   	$content = $_POST["content"];
-  	$db->query("UPDATE answer SET content = '$content' WHERE q_id = $id AND u_id = $u_id");
-  	header("Location: question.php?id=$id");
+  	try{
+  		$db->query("UPDATE answer SET content = '$content' WHERE q_id = $id AND u_id = $u_id");
+  		header("Location: question.php?id=$id");
+    } catch(PDOException $e){
+        echo $e -> getMessage();
+    }
 ?>
