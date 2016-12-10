cbd9842f99c416579abc852eda66786234e268a4
오용택 <ka123ak1@gmail.com>
2016-12-10 16:39:51 +0900

    no message

--- public/js/question.js
+++ public/js/question.js
@@ -42,10 +42,36 @@ function questionReady(){
 		form.append(contentInput);
 		form.append(idInput);
 		form.append(submitInput);
+		submitInput.on("click", modifyAjax);
 		div.empty();
 		div.append(form);
 	});
+
+
 };
+
+function modifyAjax(event) {
+
+	$("#submitInput").click(function (event){
+    var params = $(this).parent().serialize();
+    console.log(params);
+    $.ajax({
+      url: "modify_comment.php",
+      type : "POST",
+      data: params,
+      contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+      dataType: "json"
+    }).done(function (da) {
+      // var da = $.parseJSON(data);
+      if(da.error == "true"){
+        alert("수정 에러! X(");
+      } else {
+				window.location.href = document.location.href;
+			}
+    });
+  });
+}
+
 
 function appendComment(da,comment){
 	console.log(comment);
