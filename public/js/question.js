4b71847f5c3fc6db9b9861f84f6643c55918aee3
오용택 <ka123ak1@gmail.com>
2016-12-10 15:50:26 +0900

    no message

--- public/js/question.js
+++ public/js/question.js
@@ -31,7 +31,7 @@ function questionReady(){
 		var idInput = document.createElement("input");
 		idInput.setAttribute("name", "id");
 		var commentId = div.find("span.hidden").innerHTML;
-		idInput.val(commentId);
+		idInput.innerHTML = commentId;
 		idInput.setAttribute("type","hidden");
 		var submitInput = document.createElement("input");
 		submitInput.setAttribute("class", "btn commentModify submit");
