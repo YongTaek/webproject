0d7310b6088f2e35294ecc43c4b50f9e2f4fee9a
ChoiSunYeong <vmffkxlgnqh1@gmail.com>
2016-12-11 02:28:59 +0900

    include 강의 목록

--- lecture/list.php
+++ lecture/list.php
@@ -1,10 +1,4 @@
-<?php
-  session_start();
-  $logged_in = false;
-  if (isset($_SESSION["id"]) && isset($_SESSION["name"]) && isset($_SESSION["auth"])) {
-    $logged_in = true;
-  }
-?>
+<?php include("./common/pusher.php"); ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,12 +7,9 @@
   <link rel="stylesheet" href="/public/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <link rel="stylesheet" href="/public/css/lecture-list.css" type="text/css">
   <link rel="stylesheet" href="/public/css/base.css" type="text/css">
-  <script type="text/javascript">
-		<?php if (isset($_SESSION["id"]) && isset($_SESSION["favQuestion"]) && isset($_SESSION["openLecture"])) { ?>
-			var questionArray = <?php echo json_encode($_SESSION["favQuestion"]); ?>;
-			var lectureArray = <?php echo json_encode($_SESSION["openLecture"]); ?>;
-		<?php } ?>
-	</script>
+
+  <?php include("./common/script.php"); ?>
+
   <script src="/public/js/jquery-3.1.1.min.js" type="text/javascript"></script>
   <link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet" type="text/css">
   <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
@@ -28,43 +19,7 @@
   <script src="/public/js/base.js"></script>
 </head>
 <body>
-  <header role = "banner" class="banner-color">
-    <nav role="navigation">
-      <div id="logo" class="pull-left"><a href="/php/main.php"><img class="logo" src="/public/img/selab_logo_S.png"/></a></div>
-      <ul id="menu" class="inline-list pull-left">
-      <li class="pull-left"><a href="/php/noticelist.php" class="menu-item" >NOTICE</a></li>
-        <li class="pull-left"><a href="/php/questionlist.php" class="menu-item">QUESTION</a></li>
-        <li class="pull-left"><a href="/php/freelist.php" class="menu-item">BOARD</a></li>
-        <li class="pull-left"><a href="/php/lecture-list.php" class="menu-item active">LECTURE</a></li>
-      </ul>
-      <div role="login" class="pull-right">
-        <?php if ($logged_in) { ?>
-          <a id="login" href="logout.php" class='pull-right'>LOGOUT</a>
-          <div class="pull-right vr"></div>
-        <?php
-          if ($_SESSION["auth"] == "professor" || $_SESSION["auth"] == "assistant") {
-            $href = "/php/setting.php";
-          } else {
-            $href = "/php/changepw.php";
-          }
-        ?>
-          <a id="mypage" href="<?= $href ?>" class='pull-right'><?= $_SESSION["name"] ?> (<?= $_SESSION["auth"] ?>)</a>
-        <?php } else { ?>
-          <a id="login" href="dologin.php" class='pull-right'>LOGIN</a>
-        <?php } ?>
-      </div>
-      <button class="pull-right">
-        <img src="/public/img/search.png" class="search-icon">
-      </button>
-      <form method ="post" id = "search-content" action="/php/search-page.php">
-      <input type="text" class="pull-right search" name="search">
-      </form>
-    </nav>
-    <div class = "jumbotron banner-color">
-      <h1 class="align-center">Lecture</h1>
-      <p class="lead align-center">Wed 3:30 ~ & Thu 10:30 ~ </p>
-    </div>
-  </header>
+  <?php include("./common/header.php"); ?>
     <div class="container">
       <?php
       $db = new PDO("mysql:dbname=qna;host=localhost;charset=utf8", "root", "root");
