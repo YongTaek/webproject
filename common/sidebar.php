7b1ba432278ab9ab6d447c267f70574362b7f533
Cheon Yu Jung <chyjis@naver.com>
2016-12-11 03:04:45 +0900

    no message

--- common/sidebar.php
+++ common/sidebar.php
@@ -1,6 +1,7 @@
 <div id="mySidenav" class="sidenav">
   <span class="text">Notification</span>
   <a href="javascript:void(0)" class="closebtn">&times;</a>
+  <a class="all-notification-delete">모든 알림 삭제</a>
   <hr>
   <!-- <span class="no-notification">받지 않은 알림이 없습니다!</span> -->
   <div id="notifications">
