289d1fcf4b2e788374b08d0360f4627f38822a72
ChoiSunYeong <vmffkxlgnqh1@gmail.com>
2016-12-10 13:42:08 +0900

    adf

--- php/load-thread.php
+++ php/load-thread.php
@@ -10,7 +10,7 @@
   try {
     $db = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $comments = $db->query("SELECT c.id, c.content, c.time, u.name from comment c join lecture l on c.type='lecture' and l.id=c.reference_id join user u on u.id=c.u_id where c.reference_id=$id, c.time < $time order by time desc limit 10 ");
+    $comments = $db->query("SELECT c.id, c.content, c.time, u.name from comment c join lecture l on c.type='lecture' and l.id=c.reference_id join user u on u.id=c.u_id where c.reference_id=$r_id, c.time < $time order by time desc limit 10 ");
       $arrays = array();
       foreach ($comments as $comment ) {
           $userName = $comment['name'];
