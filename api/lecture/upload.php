541be5bdc17605f7308e93c6fc940e73abe43b48
gwpark <pakgunwoo@gmail.com>
2016-12-11 02:24:57 +0900

    upload.php

--- api/lecture/upload.php
+++ api/lecture/upload.php
@@ -10,7 +10,7 @@
 		if(isset($_FILES['upload']['name']) && $_POST['url'] !== "") {
 			$result = array("error" => "true");
 		} else if(isset($_FILES['upload']['name'])) {
-			$uploaddir = "../files/";
+			$uploaddir = "../../files/";
 			$fileUrl = $uploaddir . basename($_FILES['upload']['name']);
 			if(move_uploaded_file($_FILES['upload']['tmp_name'],$fileUrl)){
 				$dbUrl = $fileUrl;
