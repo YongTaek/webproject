7e2c44907ac14eab67906b131ada6eaf238992fa
ChoiSunYeong <vmffkxlgnqh1@gmail.com>
2016-12-12 20:50:00 +0900

    asdf

--- notice/delete.php
+++ notice/delete.php
@@ -12,7 +12,9 @@
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $url = $db->query("SELECT url FROM notice WHERE id = $id");
   $url = $url->fetch();
-  unlink($url);
+if($url["url"] !== NULL){
+  unlink($url["url"]);
+}
 	$db->query("DELETE FROM notice WHERE id = $id");
 	$db->query("DELETE FROM comment WHERE reference_id = $id AND type = 'notice'");
 	header("Location: /board/notice/list.php");
