6cb851feb95198e38603c9a2011ae6b7729971ef
ChoiSunYeong <vmffkxlgnqh1@gmail.com>
2016-12-13 05:30:01 +0900

    adf

--- public/js/notice.js
+++ public/js/notice.js
@@ -2,6 +2,7 @@ function ready () {
  $("button").click( function(){
   window.location.href = "/board/notice/list.php";
 });
+
  $("input.delete").click(function(){
   $("#is-click").val(1);
   $("p.origin-file").remove();
@@ -15,6 +16,7 @@ function ready () {
 $(document).ready(ready);
 
 function submitData() {
+
   $("#notice-form").ajaxForm({
     dataType: 'text',
     success: function(responseText, statusText){
@@ -22,12 +24,15 @@ function submitData() {
       var result = JSON.parse(responseText);
       if(result["error"] !== "false"){
         alert(result["message"]);
-      } else{
+      }
+      else{
         window.location.href = "/board/notice/list.php";
-      }};
+      }
+    }
   });
-}
+}  
 function modifySubmitData() {
+
   var location = window.location.search.split("=")[1];
 
   $("#notice-modify-form").ajaxForm({
