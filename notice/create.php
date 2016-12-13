da32e88b659149cd164b561a2b960dcd050da164
ChoiSunYeong <vmffkxlgnqh1@gmail.com>
2016-12-13 16:34:27 +0900

    ㅂㄷㅂㄷ

--- notice/create.php
+++ notice/create.php
@@ -31,11 +31,10 @@ if(!isset($result)){
     $db = new PDO("mysql:dbname=qna;host=localhost;charset=utf8", "root", "root");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     if($dbUrl == NULL){
-      $result = array("error" => "true", "message"=>"null!");
       $db->query("INSERT INTO notice(u_id, title, content, time, url) VALUES($id, '$title', '$content', '$time', 'NULL')");  
     }
     else{
-      $result = array("error" => "true", "message"=>"file?");
+      // $result = array("error" => "true", "message"=>"file?");
       $db->query("INSERT INTO notice(u_id, title, content, time, url) VALUES($id, '$title', '$content', '$time','$dbUrl')");  
     }
     $rows = $db->query("SELECT id FROM notice WHERE u_id=$id AND title='$title' AND content='$content' AND time='$time'");
@@ -44,7 +43,7 @@ if(!isset($result)){
       $result = array("error" => "false", "message"=>"","id"=>$row["id"]);
     }
   } catch (PDOException $e) {
-    // $result = array("error" => "true", "message"=>"파일 업로드에 실패했습니다!!! :(");
+    $result = array("error" => "true", "message"=>"파일 업로드에 실패했습니다!!! :(");
   }
 }
 print json_encode($result);
