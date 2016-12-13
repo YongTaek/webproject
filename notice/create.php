a50f604cd94656d917ff30ae5bfbabe631578144
ChoiSunYeong <vmffkxlgnqh1@gmail.com>
2016-12-13 16:46:40 +0900

    asdf

--- notice/create.php
+++ notice/create.php
@@ -11,7 +11,19 @@ $title = htmlspecialchars($_POST["title"], ENT_QUOTES);
 $content = $_POST["content"];
 
 if(!isset($_FILES['upload']['name'])){
-  $dbUrl = NULL;
+  try{
+    $db = new PDO("mysql:dbname=qna;host=localhost;charset=utf8", "root", "root");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->query("INSERT INTO notice(u_id, title, content, time, url) VALUES($id, '$title', '$content', '$time',NULL)");
+    $rows = $db->query("SELECT id FROM notice WHERE u_id=$id AND title='$title' AND content='$content' AND time='$time'");
+    if ($rows->rowCount() > 0) {
+      $row = $rows->fetch();
+      $result = array("error" => "false", "message"=>"","id"=>$row["id"]);
+    }
+  }catch(PDOException $e){
+    $result = array("error" => "true", "message"=>"파일 업로드에 실패했습니다!!! :(");
+  }
+  
 }
 else{
   $uploaddir = "../files/";
@@ -24,27 +36,23 @@ else{
     if (!($_SESSION["auth"] === "professor" || $_SESSION["auth"] === "assistant")) {
       $result = array("error" => "true", "message"=>"권한이 없습니다!");
     }
+    else{
+      try{
+        $db = new PDO("mysql:dbname=qna;host=localhost;charset=utf8", "root", "root");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->query("INSERT INTO notice(u_id, title, content, time, url) VALUES($id, '$title', '$content', '$time','$dbUrl')");
+        $rows = $db->query("SELECT id FROM notice WHERE u_id=$id AND title='$title' AND content='$content' AND time='$time'");
+        if ($rows->rowCount() > 0) {
+          $row = $rows->fetch();
+          $result = array("error" => "false", "message"=>"","id"=>$row["id"]);
+        }
+      }catch(PDOException $e){
+        $result = array("error" => "true", "message"=>"파일 업로드에 실패했습니다!!! :(");
+      }
+    }
   }
 }
 if(!isset($result)){
-  try {
-    $db = new PDO("mysql:dbname=qna;host=localhost;charset=utf8", "root", "root");
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    if($dbUrl == NULL){
-      $db->query("INSERT INTO notice(u_id, title, content, time) VALUES($id, '$title', '$content', '$time')");  
-    }
-    else{
-      // $result = array("error" => "true", "message"=>"file?");
-      $db->query("INSERT INTO notice(u_id, title, content, time, url) VALUES($id, '$title', '$content', '$time','$dbUrl')");  
-    }
-    $rows = $db->query("SELECT id FROM notice WHERE u_id=$id AND title='$title' AND content='$content' AND time='$time'");
-    if ($rows->rowCount() > 0) {
-      $row = $rows->fetch();
-      $result = array("error" => "false", "message"=>"","id"=>$row["id"]);
-    }
-  } catch (PDOException $e) {
-    $result = array("error" => "true", "message"=>"파일 업로드에 실패했습니다!!! :(");
-  }
+  print json_encode($result);  
 }
-print json_encode($result);
 ?>
