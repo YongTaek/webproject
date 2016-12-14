a58912fc56e5930d131d608e00008c47dbedc02a
오용택 <ka123ak1@gmail.com>
2016-12-14 16:05:02 +0900

    no message

--- common/sidebar.php
+++ common/sidebar.php
@@ -4,15 +4,15 @@
   <a id="all-delete" class="all-notification-delete">모든 알림 삭제</a>
   <hr class="sidebar-hr">
   <?php if(count($pushArray) == 0){ ?>
-  
+
     <span class="no-notification">받지 않은 알림이 없습니다!</span>
-  
+
   <?php }?>
   <div id="notifications">
     <?php
     foreach ($pushArray as $array ) {
     ?>
-    <a class="notification" zhref="<?=$array["url"]?>">
+    <a class="notification" href="<?=$array["url"]?>">
       <div class="notification-thread">
         <p class="notification-text"><?=$array["message"]?></p>
         <p class="notification-text"><?=$array["time"]?></p>
