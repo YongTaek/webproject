8e1a55b0a25b29e4d4cb650699f643177ac1182d
오용택 <ka123ak1@gmail.com>
2016-12-14 14:46:15 +0900

    no message

--- public/js/lecture.js
+++ public/js/lecture.js
@@ -1,10 +1,13 @@
 function lectureReady(){
-  $(".auth").parent().on("click", function (event) {
-    var name = this.parent().find(".realname");
-    var writer = this.parent().find(".writer");
-    wirter.text(name.text());
-    console.log("adsf");
-  })
+  var auths = $(".auth");
+  for(var i = 0; i < auths.length; i++) {
+    auths[i].parent().on("click", function (event) {
+      var name = this.parent().find(".realname");
+      var writer = this.parent().find(".writer");
+      wirter.text(name.text());
+      console.log("adsf");
+    })
+  }
   $("#submit").click(function (event){
     var params = $(this).parent().serialize();
     console.log(params);
